b5b08e9b794cc394da08182e0756d343e463331d
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-03-28 11:00:23 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,25 @@
 import { Routes } from '@angular/router';
 import { Task } from './pages/task/task';
 import { TastForm } from './pages/tast-form/tast-form';
+import { Login } from './pages/login/login';
+import { Loginform } from './component/loginform/loginform';
+import { SignIn } from './pages/sign-in/sign-in';
 
 export const routes: Routes = [
     {
-        path:'',
+        path:"",
+        component: Login
+    },
+    {
+        path:"sign_in",
+        component: SignIn
+    },
+    {
+        path:'task',
         component: Task
     },
     {
-        path:'tasks',
+        path:'tasksform',
         component: TastForm
     }
 ];
