1cec9be31f5e6dcc5f3219ccf4e0a1b457a86c1d
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-03-28 11:01:25 +0530

    Create sign-in.ts

---
+++ src/app/pages/sign-in/sign-in.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SginInForm } from "../../component/sgin-in-form/sgin-in-form";
+
+@Component({
+  selector: 'app-sign-in',
+  imports: [SginInForm],
+  templateUrl: './sign-in.html',
+  styleUrl: './sign-in.css',
+})
+export class SignIn {
+
+}
