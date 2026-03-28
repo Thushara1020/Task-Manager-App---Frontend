cb47f3a07c9fa64a84aa93ef1f9f1b916dcab0f9
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-03-28 11:01:12 +0530

    Create login.ts

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Loginform } from '../../component/loginform/loginform';
+
+
+@Component({
+  selector: 'app-login',
+  imports: [Loginform],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+
+}
